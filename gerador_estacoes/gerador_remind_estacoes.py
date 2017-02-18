5e6ed0c4405be236e87001ca1ccb670c5398e177
AndresMRM <andres@inventati.org>
2017-02-18 13:48:13 -0200

    seasons generator for remind

---
+++ gerador_estacoes/gerador_remind_estacoes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Gera um arquivo Remind com estações do ano.
+"""
+
+import ephem
+from pytz import timezone
+
+
+def formata_data(r):
+    return timezone('UTC').localize(r.datetime()).astimezone(
+        timezone('Brazil/East')).date()
+
+
+if __name__ == '__main__':
+    arq = open('estacoes.remind', 'w')
+    for ano in range(2000, 2051):
+        ano = str(ano)
+        print(ano)
+        estacoes = [
+            (ephem.next_solstice(ano), 'Inverno'),
+            (ephem.next_solstice(ano + '/7'), 'Verão'),
+            (ephem.next_equinox(ano), 'Primavera'),
+            (ephem.next_equinox(ano + '/7'), 'Outono'),
+        ]
+        for data, msg in estacoes:
+            arq.write('REM {data} MSG {msg}\n'.format(
+                data=formata_data(data), msg=msg))
+    arq.close()
